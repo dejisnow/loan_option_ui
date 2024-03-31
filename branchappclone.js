c72ca22eddb790330493c28bb28fbdc9e1e21941
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-03-31 23:09:57 +0100

    Update branchappclone.js

--- branchappclone.js
+++ branchappclone.js
@@ -1,18 +1,21 @@
 window.onload=()=>{
   
-  let option= document.querySelectorAll(".option")
+  let options= document.querySelectorAll(".option")
   
   
-  for(let i = 0; i < option.length; i++){
-   option[i].addEventListener("click",()=>{
-    
-    if(option[i].classList.contains("active")){
-        option[i].style.scale= 1
-    }
-    option[i].classList.toggle("active")
- 
-   })
-  }
+  options.forEach((option)=>{
+  
+  option.addEventListener("click",()=>{
+  options.forEach((element)=>{
+     element.classList.remove("active")
+  })
+      option.classList.add("active")
+  })
+  
+      
+  } )
+  
+  
    
   
   
